3ee307a3ea28c9e10fc47d94603a01b01f41b338
Ekkachai Danwanichakul <ekka21@gmail.com>
2016-07-13 09:00:46 -0500

    master: adding notes field

--- resources/views/vendors/edit.blade.php
+++ resources/views/vendors/edit.blade.php
@@ -143,6 +143,18 @@
                     @endif
                 </div>
             </div>
+             <div class="row">
+                <div class="col-xs-12 form-group">
+                    {!! Form::label('notes', 'Notes', ['class' => 'control-label']) !!}
+                    {!! Form::textarea('notes', old('notes'), ['class' => 'form-control editor']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('notes'))
+                        <p class="help-block">
+                            {{ $errors->first('notes') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
 
         </div>
     </div>
