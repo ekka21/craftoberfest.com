8bb9db2b366f063772b19b3fd392de0a3dd7165f
Ekkachai Danwanichakul <ekka21@gmail.com>
2016-07-13 08:46:24 -0500

    master: adding craft vendorinfo

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -33,6 +33,13 @@
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="row">
+                <div class="col-xs-12">
+                    <h2>Craftoberfest 2016<h1>
+                    <p>Vendor application</p>
+                    <p>This year night market will be Thursday October 13th, 2016 from 5PM to 10PM at Urbun Chestnut Brewing Company(Midtown) in St. Louis, MO.</p>
+                </div>
+            </div>
+            <div class="row">
             <div class="col-xs-12 form-group <?php if ($errors->has('full_name')) print 'has-error';?>">
                     {!! Form::label('full_name', '*Full Name', ['class' => 'control-label']) !!}
                     {!! Form::text('full_name', old('full_name'), ['class' => 'form-control']) !!}
