3c872a86a6057ab4a08bb4d7b892e5eeb84dcd6d
Ekkachai Danwanichakul <ekka21@gmail.com>
2016-07-30 12:55:46 -0500

    master: wip

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -24,17 +24,16 @@
 <!--[if lt IE 9]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
-<div class="hamburger">&#9776;</div>
-<div class="navMobile">
-<ul>
-<li><a href="/about">About</a></li>
-<li><a href="/2015-vendors">Vendors</a></li>
-<li><a href="/participate">Participate</a></li>
-<li><a href="craftoberfest2015@gmail.com">Contact</a></li>
-</ul>
-</div>
+<!-- <div class="hamburger">&#9776;</div> -->
+<!-- <div class="navMobile">
+	<ul>
+	<li><a href="/about">About</a></li>
+	<li><a href="/2015-vendors">Vendors</a></li>
+	<li><a href="/participate">Participate</a></li>
+	<li><a href="craftoberfest2015@gmail.com">Contact</a></li>
+	</ul>
+</div> -->
 <div class="container">
-	
 	@yield('content')
 </div>
 <script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script></body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.front')
 
 @section('content')
-<div class="nav">
-<ul>
-<li><a href="/about">About</a></li>
-<li><a href="/2015-vendors">Vendors</a></li>
-<li><a href="/participate">Participate</a></li>
-<li><a href="mailto:craftoberfest2015@gmail.com">Contact</a></li>
-</ul>
+<!-- <div class="nav">
+	<ul>
+		<li><a href="/about">About</a></li>
+		<li><a href="/2015-vendors">Vendors</a></li>
+		<li><a href="/participate">Participate</a></li>
+		<li><a href="mailto:craftoberfest2015@gmail.com">Contact</a></li>
+	</ul>
+</div> -->
+<div class="lights">
+	<img src="images/lights_2016.svg" alt=""/>
 </div>
-<div class="lights"><img src="images/lights.svg" alt=""/></div>
 <div class="inner">
-<p><img class="date" src="images/date.svg" alt=""/></p>
-<p><a href="/"><img class="logo" src="images/logo.svg" alt=""/></a></p>
-<p><img class="indie" src="images/indie.svg" alt=""/></p>
-<p><img class="fox" src="images/fox.svg" alt=""/></p>
-<p><img class="handmade" src="images/locallyHandmade.svg" alt=""/></p>
-<p>
-<a href="https://www.facebook.com/stlcraftoberfest" target="_blank"><img src="images/fb.png" alt=""/></a>
-<a href="https://twitter.com/Craftoberfest" target="_blank"><img src="images/tw.png" alt=""/></a>
-</p>
+	
+	<p><a href="/"><img class="logo" src="images/logo_2016.svg" alt=""/></a></p>
+	<ul class="main_menu">
+		<li><a href="/about">About</a></li>
+		<li><a href="/2016-vendors">Vendors</a></li>
+		<li><a href="/participate">Participate</a></li>
+		<li><a href="mailto:craftoberfest2016@gmail.com">Contact</a></li>
+	</ul>
+	<p><img class="handmade" src="images/craft_beer_craft_goods.svg" alt=""/></p>
+
+	<p>
+		<a href="https://www.facebook.com/stlcraftoberfest" target="_blank"><img src="images/fb.png" alt=""/></a>
+		<a href="https://twitter.com/Craftoberfest" target="_blank"><img src="images/tw.png" alt=""/></a>
+	</p>
 </div>
 @endsection
